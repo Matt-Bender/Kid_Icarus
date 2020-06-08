247e41357810ab15a320b551be6139a4e092f8dc
Husky Fusky <bendermaj@gmail.com>
2020-06-08 16:49:15 -0400

    Revert "Revert "menus/spawning player""
    This reverts commit 6a9763fcc5d716c2b82a323562ffdb316acf22a1.

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -5,16 +5,29 @@ using UnityEngine;
 public class Level : MonoBehaviour
 {
     public Transform spawnPoint;
+    public GameObject playerPrefab;
+
+    private bool spawnPlayer = false;
     // Start is called before the first frame update
     void Start()
     {
         GameManager.instance.lives = 3;
-        GameManager.instance.SpawnPlayer(spawnPoint);
-    }
+        //    GameManager.instance.DestroyPlayer();
+
+        if (playerPrefab && spawnPoint)
+        {
+            // Instantiate (Create) 'Character' GameObject
+            GameManager.instance.SpawnPlayer(spawnPoint);
+        }
+        else
+            // Prints a message to Console (Shortcut: Control+Shift+C)
+            Debug.LogError("Missing Player Prefab or SpawnPoint");
+
+}
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
